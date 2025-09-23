78aeaf98143c7f9f017b9bbaccd6864b30b5b9cf
Dom <domeccleston@gmail.com>
2025-09-23 23:58:49 +0100

    port to rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,13 +1,15 @@
-mod cell;
-use cell::{Cell, CellConfig};
+mod game_of_life;
+use game_of_life::GameOfLife;
 
 fn main() {
-    let my_cell = Cell::from_config(CellConfig {
-        id: 0,
-        x: 1,
-        y: 1,
-        alive: true,
-    });
+    let mut game = GameOfLife::from_random_grid(10, 10, 0.3);
 
-    println!("{:?}", my_cell);
+    for generation in 0..101 {
+        println!("Generation {}", generation);
+        game.print();
+        game.step();
+
+        // Add a small delay if you want
+        std::thread::sleep(std::time::Duration::from_millis(200));
+    }
 }
